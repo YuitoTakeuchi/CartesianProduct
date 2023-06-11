4cf6ab8f281691986fdc6cb7333aa2c6a68a3e60
YuitoTakeuchi <tyuito0913@gmail.com>
2023-06-11 23:34:29 +0900

    feat add random

--- include/CartesianProduct.hpp
+++ include/CartesianProduct.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <iostream>
 #include <array>
 #include <vector>
 #include <cmath>
@@ -8,12 +7,19 @@
 #include <numeric>
 #include <functional>
 
+#include <random>
+
 template<int... N>
 class CartesianProductIndex {
     static constexpr int dimension = sizeof...(N);
     static constexpr int sizes[dimension] = {N...};
     static constexpr long prod = std::accumulate(std::begin(sizes), std::end(sizes), 1UL, std::multiplies<long>{});
     std::array<int, dimension> product_set{};
+    std::array<int, dimension> _product_set{}; // for random get
+
+    std::random_device seed_gen;
+    std::mt19937 engine = std::mt19937(seed_gen());
+    std::uniform_int_distribution<> dist = std::uniform_int_distribution<> (0, prod);
 
 public:
     CartesianProductIndex() {
@@ -27,6 +33,14 @@ public:
         return product_set;
     }
 
+    const std::vector<int>& random() {
+        long idx = dist(engine);
+        for(int i = 0; i < dimension; ++i) {
+            _product_set[i] = idx % sizes[i];
+        }
+        return _product_set;
+    }
+
     long size() {return prod;}
 };
 
@@ -37,9 +51,14 @@ class CartesianProductIndex<> {
     long prod;
     std::vector<int> sizes;
     std::vector<int> product_set;
+    std::vector<int> _product_set; // for random get
+    std::random_device seed_gen;
+    std::mt19937 engine = std::mt19937(seed_gen());
+    std::uniform_int_distribution<> dist;
 
 public:
     CartesianProductIndex(int size, int dimension) {
+        dist = std::uniform_int_distribution<> (0, prod);
         this->dimension = dimension;
         sizes = std::vector<int> (dimension, size);
         prod = pow(size, dimension);
@@ -53,5 +72,14 @@ public:
         }
         return product_set;
     }
+
+    const std::vector<int>& random() {
+        long idx = dist(engine);
+        for(int i = 0; i < dimension; ++i) {
+            _product_set[i] = idx % sizes[i];
+        }
+        return _product_set;
+    }
+
     long size() {return prod;}
 };
